0ab23d6392206328ced6e7d6d067f76a859c4479
zukizukimaru <kazucchi.98323@gmail.com>
2020-01-31 19:33:44 +0900

    アロー関数を使って式を短く書いてみた。

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 'use strict';
 
-// function sum(a,b,c) {
-//   // console.log(a + b + c);
-//   return a + b +c;
-// }
 
-const sum = function(a, b, c) {
-  return a + b +c;
-};
+// const sum = (a, b, c) => a + b +c;
 
-// sum(1, 2, 3);
-// sum(3, 4,5);
 
-const total =sum(1, 2, 3) + sum(3, 4, 5); //18
-console.log(total);
+// const total =sum(1, 2, 3) + sum(3, 4, 5); //18
+// console.log(total);
+
+// const double = function(a) {
+//   return a * 2;
+// };
+
+const double = a => a * 2;
+console.log(double(12));
