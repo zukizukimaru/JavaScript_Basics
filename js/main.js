0c9bd112c9c33b63f14484ac0d0a4c10e4d7e874
zukizukimaru <kazucchi.98323@gmail.com>
2020-01-31 19:09:05 +0900

    引数を使ってみた。

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 'use strict';
 
- function showAd()  {
+ function showAd(message = 'Ad')  { //仮引数
    console.log('----------');
-   console.log('----Ad----');
+   console.log(`---- ${message} ----`);
    console.log('----------');
  }
 
- showAd();
+ showAd('Header Ad');  //実引数
  console.log('Tom is great!');
  console.log('Bob is great!');
+//  showAd('Ad');
  showAd();
  console.log('Steab is great!');
  console.log('Richard is great!');
- showAd();
+ showAd('Footer Ad');
  
