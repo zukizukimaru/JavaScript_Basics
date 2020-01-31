79e57121f8e32a3386158534ea827589470ac576
zukizukimaru <kazucchi.98323@gmail.com>
2020-01-31 19:26:18 +0900

    関数式を使ってみた

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 'use strict';
 
-function sum(a,b,c) {
-  // console.log(a + b + c);
+// function sum(a,b,c) {
+//   // console.log(a + b + c);
+//   return a + b +c;
+// }
+
+const sum = function(a, b, c) {
   return a + b +c;
-}
+};
 
 // sum(1, 2, 3);
 // sum(3, 4,5);
