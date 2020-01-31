7bd4c142814c3d5dc1d39a310f7fe7387f2902b1
zukizukimaru <kazucchi.98323@gmail.com>
2020-01-31 17:49:12 +0900

    for文を使った繰り返しの処理

--- js/main.js
+++ js/main.js
@@ -1,21 +1,7 @@
 'use strict';
 
-const score = 60;
-const name ='kazuki';
-
-// if (score >= 50) {
-//   if(name === 'kazuki') {
-//     console.log('Good job!');
-//   }
-// }
-
-
-// 論理演算子について
-//&& なおかつ(AND)
-// || もしくは(OR)
-//! 〜ではない(NOT)
-
-
-if (score >= 50 && name === 'kazuki') {
-  console.log('Good job!');
+for (let i = 1; i <= 10; i++) {
+  // console.log('hello');
+  // console.log('hello' + i);
+  console.log(`hello $(i)`);
 }
