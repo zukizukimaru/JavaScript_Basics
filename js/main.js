060d006c1a90b551de1c64951abe0362a149e25c
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-01 17:25:03 +0900

    pushやshiftを使い配列を操作した

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 'use strict';
 
 {
-  const scores = [80, 90, 40];
+  const scores = [80, 90, 40, 70];
+  scores.push(60, 50);
+  scores.shift();
+  // 90, 40, 70, 60, 50
   
-  // console.log(`Score: ${scores[0]}`);
-  // console.log(`Score: ${scores[1]}`);
-  // console.log(`Score: ${scores[2]}`);
-
-  // for (let i = 0; i < 3; i++) {
     for (let i =0; i < scores.length; i++) {
       console.log(`Score ${i}; ${scores[i]}`);
     }
