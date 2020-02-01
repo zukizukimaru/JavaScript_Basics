875d0e6eba1e2cd6e97c1026f13f16d31fb60a06
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-01 18:06:38 +0900

    forEachを使って文をすっきりと書いてみた

--- js/main.js
+++ js/main.js
@@ -4,20 +4,9 @@
 
   const scores = [80, 90, 40, 70,];
   
-  // const [a, b, c, d] = scores;
-  // console.log(a);
-  // console.log(b);
-  // console.log(c);
-  // console.log(d);
-
-  // const [a, b, ...others] = scores;
-  // console.log(a);
-  // console.log(b);
-  // console.log(others);
-
-  let x = 30;
-  let y = 70;
-  [x, y] = [y,x];
-  console.log(x);
-  console.log(y);
+  // scores.forEach((score) => {
+  scores.forEach((score, index) => {
+      console.log(`Score ${index}: ${score}`); 
+  });
+  
 }
