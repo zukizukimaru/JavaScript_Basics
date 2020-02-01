5563872f920e57ada2b3b32d6a9e902ec7ace652
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-01 18:59:12 +0900

    オブジェクトを作ってみた

--- js/main.js
+++ js/main.js
@@ -1,19 +1,12 @@
 'use strict';
 
 {
+  // const point = [100, 180];
 
-  const numbers = [1, 4, 7, 8, 10];
-
-  // const evebNumbers = numbers.filter(number => {
-  //   if (number % 2 === 0) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // });
-
-  const evebNumbers = numbers.filter(number => number % 2 === 0);
-  
-  console.log(evebNumbers);
-
+  // const point = {x: 100, y: 180};
+  const point = {
+    x: 100,
+    y: 180,
+  };
+  console.log(point);
 }
