58a99a5411a6e6590b8feaad1565875e3d383a7e
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-01 17:29:37 +0900

    spliceを使い要素を追加した

--- js/main.js
+++ js/main.js
@@ -2,9 +2,7 @@
 
 {
   const scores = [80, 90, 40, 70];
-  scores.push(60, 50);
-  scores.shift();
-  // 90, 40, 70, 60, 50
+  scores.splice(1, 1, 40, 50)
   
     for (let i =0; i < scores.length; i++) {
       console.log(`Score ${i}; ${scores[i]}`);
