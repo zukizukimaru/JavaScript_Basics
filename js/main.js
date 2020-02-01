e456e44990dc619b1de6d9ce07056d1d3233ddba
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-01 19:15:45 +0900

    オブジェクトを操作した

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 'use strict';
 
 {
- 
+ const otherProps = {
+   r: 4,
+   color: 'red',
+ };
+
   const point = {
     x: 100,
     y: 180,
+    ...otherProps,
   };
-   point.x = 120;
-   //point['x'] = 120;
+  //  console.log(point);
 
-  //  console.log(point.x);
-  //  console.log(point['y']);
-
-  point.z = 90;
-
-delete point.y;
-console.log(point);
+ const {x, r, ...others} = point;
+ console.log(x); 
+ console.log(r); 
+ console.log(others); 
 }
