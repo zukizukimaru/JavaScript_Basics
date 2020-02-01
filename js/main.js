2148de3ee2d190250fc0075689395a9d6fc9404f
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-01 19:06:41 +0900

    オブジェクトを操作した。

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 'use strict';
 
 {
-  // const point = [100, 180];
-
-  // const point = {x: 100, y: 180};
+ 
   const point = {
     x: 100,
     y: 180,
   };
-  console.log(point);
+   point.x = 120;
+   //point['x'] = 120;
+
+  //  console.log(point.x);
+  //  console.log(point['y']);
+
+  point.z = 90;
+
+delete point.y;
+console.log(point);
 }
