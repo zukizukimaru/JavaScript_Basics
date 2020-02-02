cd5e4a60cba5e31540f27d86c0591576852b3e8f
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-02 18:44:57 +0900

    Object.keys()を使ってキーを配列で取得した

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
 'use strict';
 
 {
- const otherProps = {
-   r: 4,
-   color: 'red',
+ const point = {
+   x: 100,
+   y: 180,
  };
+//  const keys = Object.keys(point);
+//  keys.forEach(key => {
+//    console.log(`Key: ${key} Value: ${point[key]}`);
+//  });
 
-  const point = {
-    x: 100,
-    y: 180,
-    ...otherProps,
-  };
-  //  console.log(point);
-
- const {x, r, ...others} = point;
- console.log(x); 
- console.log(r); 
- console.log(others); 
+ const points = [
+   {x: 30, y: 20},
+   {x: 10, y: 50},
+   {x: 40, y: 40},
+ ];
+ console.log(points[1].y);
 }
