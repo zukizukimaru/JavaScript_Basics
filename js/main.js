36fde9145f25bece5eabc649df704503427dca5b
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-08 19:23:42 +0900

    join,split を使って時間を表した。

--- js/main.js
+++ js/main.js
@@ -2,13 +2,16 @@
 
 {
 
-  const str = 'hello';
-  
-  // console.log(str.length);
+  const d = [2019, 11, 14];
 
-  // str.substring(開始１,終了位置);
-  // console.log(str.substring(2, 4));
+  // console.log(d.join('/'));
+  // console.log(d.join(''));
 
-  console.log(str[1]);
   
+const t = '17:08:24';
+// console.log(t.split(':'));
+const [hour,minute, second] = t.split(':');
+console.log(hour);
+console.log(minute);
+console.log(second);
 }
