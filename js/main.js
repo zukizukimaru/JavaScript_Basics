64ac6588dc83faaef34e4398e125aa6af9620f13
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-10 17:39:48 +0900

    サイコロのようにランダムな整数値を作った

--- js/main.js
+++ js/main.js
@@ -2,24 +2,13 @@
 
 {
 
- const scores = [10, 3, 9];
+// console.log(Math.random());//数字をランダムに表示
 
- let sum = 0;
-
- scores.forEach(score => {
-   sum += score;
- });
-
- const avg = sum / scores.length;
-
-//  console.log(sum);
-//  console.log(avg);
-
-// console.log(Math.floor(avg)); //7 小数点以下切り捨て
-// console.log(Math.ceil(avg)); //8　切り上げ
-// console.log(Math.round(avg)); //7　四捨五入
-// console.log(avg.toFixed(3)); //7.333　指定
-
-console.log(Math.random());//数字をランダムに表示
+// 0, 1, 2 
+// Math,floor(Math.random() * 3)
+// 0, ..., n 
+// Math.floor(Math.random() * (n + 1))
+// min, ..., max 
+console.log(Math.floor(Math.random() * 6)  + 1);
 
 }
