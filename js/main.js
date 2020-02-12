c4231e378329aa243142286c55b3b3b1433801e0
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-12 18:07:04 +0900

    クラス実装をした

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
 'use strict';
 
 {
+  class Post {
+    constructor(text) {
+    this.text = text;
+    this.likeCount = 0;
+    }
 
-  const posts = [
-    {
-      text: 'javascriptの勉強中...',
-      likeCount: 0,
-      // show: function() {
-      //   console.log(`${this.text} - ${this.likeCount}いいね`);
-      show() {
-        console.log(`${this.text} - ${this.likeCount}いいね`);
-      },
-    },
-    {
-      text: 'プログラミング楽しい！',
-      likeCount: 0,
-    },
-  ];
+  show() {
+    console.log(`${this.text} - ${this.likeCount}いいね`);
+  }
+}
 
-  // show(posts[0]);
+const posts = [
+  new Post('JavaScriptの勉強中...'),
+  new Post('プログラミング楽しい！'),
+];
+
+
 posts[0].show();
+posts[1].show();
 
 }
 
 
-  
+
 
 
 
