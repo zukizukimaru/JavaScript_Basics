5888753d9a3dc8f9ac94d87424b052afcf568abe
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-12 16:14:41 +0900

    ダイアログを表示させた。

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 'use strict';
 
 {
-const d = new Date(2019, 10); //2019/11/01 00:00:00
-d.setHours(10, 20, 30);//2019/11/01/10:20:30
-d.setDate(31);//2019/12/01 10:20:30
-d.setDate(d.getDate() + 3);//2019/12/01 10:20:30
-console.log(d);
 
+  // alert('hello');
+
+  const answer = confirm('削除しますか？');
+  if(answer) {
+    console.log('削除しました');
+  } else {
+    console.log('キャンセルしました')
+  }
 }
