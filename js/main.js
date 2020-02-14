75257e958c7aaea4d99a9ecbbd7aefd0f620c813
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-14 17:47:06 +0900

    要素の属性を操作した。

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@
 {
   
   document.querySelector('button').addEventListener('click', () => {
-    document.getElementById('target').textContent = 'Changed!';
+
+    const targetNode = document.getElementById('target');
+
+    targetNode.textContent = 'Changed!' ;
+    targetNode.title = 'This is title!' ;
+    targetNode.style.color = 'red' ;
+    targetNode.style.backgroundColor = 'skyblue' ;
+    //JavaScriptではbackground-colorと-ではなく、C大文字で表す。
+    
   });
 
 }
