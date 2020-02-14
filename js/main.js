a33db6f7c0fc31d4e7b12182e4df9173fa76c1ae
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-14 16:54:46 +0900

    複数の要素を取得し、DOMの変更を行った。

--- js/main.js
+++ js/main.js
@@ -1,46 +1,14 @@
 'use strict';
 
 {
-  class Post { // 親クラス
-    constructor(text) {
-      this.text = text;
-      this.likeCount = 0;
-    }
+  function update() {
+    // document.getElementById('target').textContent = 'Changed!';
+    // document.querySelector('p').textContent = 'Changed!';
+    // document.querySelectorAll('p')[1].textContent = 'Changed!';
+    document.querySelectorAll('p').forEach((p, index) => {
+      p.textContent = `${index}番目のpです!`;
+    });
+   }
 
-    show() {
-      console.log(`${this.text} - ${this.likeCount} likes`);
-    }
-
-    like() {
-      this.likeCount++;
-      this.show();
-
-    }
-
-  }
-  class SponsoredPost extends Post {// 子クラス
-    constructor(text, sponsor) {
-      super(text);
-      this.sponsor = sponsor;
-    }
-
-    show() {
-      super.show();
-      console.log(`... sponsored by ${this.sponsor}`);
-    }
-  }
-
-  const posts = [
-    new Post('JavaScriptの勉強中...'),
-    new Post('プログラミング楽しい！'),
-    new SponsoredPost('3分間動画でマスターしよう', 'dotinstall'),
-  ];
-
-  posts[2].show();
-  posts[2].like();
+  setTimeout(update, 1000);
 }
-
-
-
-
-
