0c710ef659e0cca4d2cba23aeb77624c84480177
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-15 19:08:58 +0900

    DOMに要素を追加して表示させた

--- js/main.js
+++ js/main.js
@@ -3,11 +3,12 @@
 {
   
   document.querySelector('button').addEventListener('click', () => {
+    const item2 = document.createElement('li');
+    item2.textContent = 'item 2';
 
-    const targetNode = document.getElementById('target');
+    // const ulNode = document.querySelector('ul');
+    const ul = document.querySelector('ul');
+    ul.appendChild(item2);
 
-    
-    // targetNode.textContent = 'Dotinstall';
-    targetNode.textContent = targetNode.dataset.translation;
   });
 }
