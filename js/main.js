eaa5882e4595036b1eb50c581b9673a6ea676b54
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-15 19:18:19 +0900

    要素の複製と挿入をした。

--- js/main.js
+++ js/main.js
@@ -3,12 +3,11 @@
 {
   
   document.querySelector('button').addEventListener('click', () => {
-    const item2 = document.createElement('li');
-    item2.textContent = 'item 2';
+    const item0 = document.querySelectorAll('li')[0];
+    const copy = item0.cloneNode(true);
 
-    // const ulNode = document.querySelector('ul');
     const ul = document.querySelector('ul');
-    ul.appendChild(item2);
-
+    const item2 = document.querySelectorAll('li')[2];
+    ul.insertBefore(copy, item2);
   });
 }
