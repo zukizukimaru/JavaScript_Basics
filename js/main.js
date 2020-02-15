773d591fd23089bd81d8e9a820fa3b1af8a54ec8
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-15 19:53:12 +0900

    input要素を作り操作した

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@
 {
   
   document.querySelector('button').addEventListener('click', () => {
-    const item1 = document.querySelectorAll('li')[1];
+    const li = document.createElement('li');
+    const text = document.querySelector('input');
+    li.textContent = text.value;
+    document.querySelector('ul').appendChild(li);
 
-    // item1.remove();
-    //親Node.removeChild(削除するNode)
-    document.querySelector('ul').removeChild(item1);
-
-
+    text.value = '';
+    text.focus();
   });
 }
