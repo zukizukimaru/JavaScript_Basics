fce4c31db9fac99241afc36c0fa9dcd54042902f
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-16 18:56:58 +0900

    consoleを使ってマウスを動かした数を表示させた。

--- js/main.js
+++ js/main.js
@@ -1,23 +1,13 @@
 'use strict';
 
 {
-  
-  document.querySelector('button').addEventListener('click', () => {
-    
-    const colors = document.querySelectorAll('input');
-    const selectedColors = [];
+  document.querySelector('button').addEventListener('dblclick' , () => {
+    console.log('Double Clicked!');
 
-    colors.forEach(color => {
-      if (color.checked === true) {
-        selectedColors.push(color.value);
-      }
-    });
-
-    const li = document.createElement('li');
-    li.textContent = selectedColors.join(',');
-    li.textContent = selectedColors;
-    document.querySelector('ul').appendChild(li);
-    
-    
   });
+
+  document.addEventListener('mousemove' , () => {
+    console.log('moved!');
+  });
+  
 }
