454482da8bf4ac0d0ec68c6451cf464da541e8ef
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-16 18:36:23 +0900

    DOMを使ってチェックボックスを操作した

--- js/main.js
+++ js/main.js
@@ -5,16 +5,17 @@
   document.querySelector('button').addEventListener('click', () => {
     
     const colors = document.querySelectorAll('input');
-    let selectedColor;
+    const selectedColors = [];
 
     colors.forEach(color => {
       if (color.checked === true) {
-        selectedColor = color.value;
+        selectedColors.push(color.value);
       }
     });
 
     const li = document.createElement('li');
-    li.textContent = selectedColor;
+    li.textContent = selectedColors.join(',');
+    li.textContent = selectedColors;
     document.querySelector('ul').appendChild(li);
     
     
